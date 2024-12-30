06a7bbd9ecae547f3b481d5988c9fbdd6ae06e63
Denis Skobelev <moriontone@gmail.com>
2024-12-30 19:33:47 +0300

    removedmaints

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import './style.css';
-import '@mdi/font/css/materialdesignicons.css';
-import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { createVuetify } from 'vuetify';
